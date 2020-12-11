56f44495d7e00ff2ef9302177d7ecc257c6c112c
secumihai <secumihai@gmail.com>
2020-12-11 14:39:13 +0200

    added new project

---
+++ src/main/java/md/secumihai/Main.java
@@ -0,0 +1,41 @@
+package md.secumihai;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Ion", "Mesina", "mesinaion@javaguides.com");
+        Student student1 = new Student("Vanea", "Buldozer", "buldozervanea@javaguides.com");
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.save(student);
+            session.save(student1);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List <Student> students = session.createQuery("From Student", Student.class).list();
+            students.forEach(s -> System.out.println(s.getFirstName()));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+}
